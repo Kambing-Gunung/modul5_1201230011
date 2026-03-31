e0a9dd32c7b94adc0b489a3029fd5cfb42766f0b
David NTB <davidntb7000@gmail.com>
2026-04-01 05:37:34 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        float a = 23;
+        float b = 00;
+        float c = 11;
+
+        SimpleDataBase<float> db = new SimpleDataBase<float>();
+        db.AddNewData(a);
+        db.AddNewData(b);
+        db.AddNewData(c);
+
+        db.PrintAllData();
+    }
+}
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}

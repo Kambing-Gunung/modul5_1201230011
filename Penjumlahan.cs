851b9d813abe8d61864c599a90243a69524ba3d2
David NTB <davidntb7000@gmail.com>
2026-04-01 04:13:57 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ Penjumlahan.cs
@@ -0,0 +1,11 @@
+class Penjumlahan
+{
+    public T JumlahTigaAngka<T>(T a, T b, T c)
+    {
+        dynamic x = a;
+        dynamic y = b;
+        dynamic z = c;
+
+        return x + y + z;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float a = 23;
+        float b = 00;
+        float c = 11;
+
+        Penjumlahan p = new Penjumlahan();
+        var hasil = p.JumlahTigaAngka(a, b, c);
+        Console.WriteLine("Hasil Penjumlahan: " + hasil);
+    }
+}
